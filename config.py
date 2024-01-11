77ef04cbbc70368ff02e31a9d0c95cdf0687296e
onlyinstaPLUS <134713123+onlyinstaPLUS@users.noreply.github.com>
2024-01-11 06:10:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 api_id = int("23160456")
 api_hash = "16f2fa81daef2829373070393b42a003"
-bot_token = "6737077384:AAFlV2e_ukPRARQDMBEAwkAJawZplT9wdfY"
+bot_token = "6310512831:AAH8lRcBcT2sJg5lbV8vNSMY-_AXmmNJAKQ"
 
 MAX_GEN = 10
 MONGOURI = "mongodb+srv://2rupaykamail:2rupaykamail@cluster0.syvrqrx.mongodb.net/?retryWrites=true&w=majority"
