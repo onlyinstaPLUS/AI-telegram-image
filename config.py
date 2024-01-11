471d7a1fab50022ece9163746ad30bf0201a4b5e
onlyinstaPLUS <134713123+onlyinstaPLUS@users.noreply.github.com>
2024-01-11 05:37:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = int("your api id")
-api_hash = "your api hash"
-bot_token = "your bot token"
+api_id = int("23160456")
+api_hash = "16f2fa81daef2829373070393b42a003"
+bot_token = "6737077384:AAFlV2e_ukPRARQDMBEAwkAJawZplT9wdfY"
 
 MAX_GEN = 10
-MONGOURI = "Your MONGO DB URI"
-OWNER_ID = int("owner telegram id")
+MONGOURI = "mongodb+srv://2rupaykamail:2rupaykamail@cluster0.syvrqrx.mongodb.net/?retryWrites=true&w=majority"
+OWNER_ID = int("6367495275")
